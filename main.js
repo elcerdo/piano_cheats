fe680b73a5d2daedb6acdfb55b0b760ce3cd42b2
Pierre Gueth <gueth@adobe.com>
2024-02-17 14:07:16 +0100

    class

--- main.js
+++ main.js
@@ -1,40 +1,57 @@
-var draw = {
-    clear: (ctx, canvas) => {
-        ctx.fillStyle = 'yellow';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-    },
+class Draw {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.context = this.canvas.getContext('2d');
+        this.origin_xx = 10;
+        this.origin_yy = 17;
+        this.text_spacing = 2;
+        this.grid_size = 10;
+        this.row_spacing = 120;
+    }
 
-    piano: (ctx, spc_xx = 10, spc_yy = 17, num_octaves = 3, hh = 10, spc_text = 2) => {
+    clear() {
+        this.context.fillStyle = 'yellow';
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    piano(row_offset, num_octaves) {
+        const spc_yy = this.origin_yy + row_offset * this.row_spacing;
+        let ctx = this.context;
+
         ctx.textAlign = 'center';
         ctx.font = '10px sans-serif';
 
         { // white notes
+            const width = this.grid_size * 3;
+            const height = this.grid_size * 8;
             ctx.textBaseline = 'top';
-            let cur_xx = spc_xx;
+            let cur_xx = this.origin_xx;
             for (let kk = 0; kk < num_octaves; kk++)
                 for (const ll of "CDEFGAB") {
                     ctx.fillStyle = 'white';
                     ctx.strokeStyle = 'black';
-                    ctx.fillRect(cur_xx, spc_yy, hh * 3, hh * 8);
-                    ctx.strokeRect(cur_xx, spc_yy, hh * 3, hh * 8);
+                    ctx.fillRect(cur_xx, spc_yy, width, height);
+                    ctx.strokeRect(cur_xx, spc_yy, width, height);
                     ctx.fillStyle = 'black';
-                    ctx.fillText(ll, cur_xx + 1.5 * hh, spc_yy + hh * 8 + spc_text);
-                    cur_xx += 3 * hh;
+                    ctx.fillText(ll, cur_xx + width / 2, spc_yy + height + this.text_spacing);
+                    cur_xx += width;
                 }
         }
 
         { // black keys
+            const width = this.grid_size * 2;
+            const height = this.grid_size * 5;
             ctx.textBaseline = 'bottom';
             const black_note = (kk, ll) => {
-                let cur_xx = spc_xx + (2 + 3 * kk) * hh;
+                let cur_xx = this.origin_xx + this.grid_size * (2 + 3 * kk);
                 for (let kk = 0; kk < num_octaves; kk++) {
                     ctx.strokeStyle = 'black';
                     ctx.fillStyle = 'black';
-                    ctx.fillRect(cur_xx, spc_yy, hh * 2, hh * 5);
-                    ctx.strokeRect(cur_xx, spc_yy, hh * 2, hh * 5);
+                    ctx.fillRect(cur_xx, spc_yy, width, height);
+                    ctx.strokeRect(cur_xx, spc_yy, width, height);
                     ctx.fillStyle = 'black';
-                    ctx.fillText(ll, cur_xx + hh, spc_yy - spc_text);
-                    cur_xx += 3 * 7 * hh;
+                    ctx.fillText(ll, cur_xx + width / 2, spc_yy - this.text_spacing);
+                    cur_xx += this.grid_size * 3 * 7;
                 }
             };
             black_note(0, "Db");
@@ -49,14 +66,14 @@ var draw = {
 ///////
 
 const canvas = document.getElementById('piano_canvas');
-const context = canvas.getContext('2d');
+const draw = new Draw(canvas);
 
 const loop = () => {
     requestAnimationFrame(loop);
-    draw.clear(context, canvas)
-    draw.piano(context);
-    draw.piano(context, 10, 127, 2);
-    draw.piano(context, 10, 247, 4);
+    draw.clear();
+    draw.piano(0, 3);
+    draw.piano(1, 2);
+    draw.piano(2, 4);
 };
 
 loop();
